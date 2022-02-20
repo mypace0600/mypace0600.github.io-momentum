39b8e4fdac03e81799a926d53a4f8dcb950030ef
mypace0600 <mypace0600@gmail.com>
2022-02-20 12:03:52 +0900

    oh

--- JS/weather.js
+++ JS/weather.js
@@ -1,11 +1,11 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "d6e6400bbaa937195a6a0053e922d9a5";
+const WEATHER_API_KEY = config.apikey;
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
